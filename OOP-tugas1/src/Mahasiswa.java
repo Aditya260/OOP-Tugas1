d94ddc3bc83bf1dc8c075878313bdabcdca056e6
Aditya260 <56083913+Aditya260@users.noreply.github.com>
2020-03-21 15:55:49 +0700

    Add files via upload

---
+++ OOP-tugas1/src/Mahasiswa.java
@@ -0,0 +1,15 @@
+
+public class Mahasiswa {
+
+    public static void main(String[] args) {
+        main mhs = new main();
+        mhs.nama = " Aditya Yudha Remarais";
+        mhs.nim = "18090021";
+        mhs.kelas = "4B";
+        
+        System.out.println("Nama = "+mhs.nama);
+        System.out.println("Nim = "+mhs.nim);
+        System.out.println("Kelas = "+mhs.kelas);
+    }
+    
+}
